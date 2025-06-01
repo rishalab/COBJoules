6d193170077b7e5b039253e6e44aea3eb8a32952
Shiva9361 <shivadharshansankar936@gmail.com>
2025-06-02 02:45:46 +0530

    fix: installer

---
+++ installer.sh
@@ -0,0 +1,3 @@
+wget https://github.com/rishalab/COBJoules/releases/download/v1.0/COBJoules.tar.gz
+tar -xvzf COBJoules.tar.gz 
+rm COBJoules.tar.gz
--- test/test.cbl
+++ test/test.cbl
@@ -3,16 +3,16 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-         01  WS-I            PIC 9(5) VALUE 0.
+        01 WS-I PIC 9(5) VALUE 0.
 
        PROCEDURE DIVISION.
-    
+
        CALL 'measure_energy'.
-       
-       PERFORM VARYING WS-I FROM 1 BY 1 UNTIL WS-I > 50000
-           CONTINUE
-       END-PERFORM
-       
+
+        PERFORM VARYING WS-I FROM 1 BY 1 UNTIL WS-I > 50000
+         CONTINUE
+        END-PERFORM
+
        CALL 'measure_energy'.
 
        STOP RUN.
